a3433115efafce6c7c4f554a782841bc01354606
Paul <paul@kerspe.com>
2014-12-17 22:19:15 +0100

    fixed namespaces

--- controllers/backend/DefaultController.php
+++ controllers/backend/DefaultController.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace schallschlucker\simplecms\controllers;
+namespace schallschlucker\simplecms\backend\controllers;
 
 use Yii;
 use yii\helpers\Url;
