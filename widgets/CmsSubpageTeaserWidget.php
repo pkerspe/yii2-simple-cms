9f9afba50e192bfc1c4ee355ed8a70e1fc4e5f97
Paul Kerspe <paul.kerspe@unbelievable-machine.com>
2016-05-11 23:23:47 +0200

    added parameter to widget allowing configuration if creation date should be displayed or not

--- widgets/CmsSubpageTeaserWidget.php
+++ widgets/CmsSubpageTeaserWidget.php
@@ -24,56 +24,58 @@ use schallschlucker\simplecms\models\CmsPageContent;
 /**
  * A widget to render a tabular listing of subpage teasers (sub pages (if any) of the menu item id given in the parameter).
  * The following parameters are supported:
- * @param $cmsHierarchyItem CmsHierarchyItem required: the hierarchy item to render the subpage teasers for 
+ * @param $cmsHierarchyItem CmsHierarchyItem required: the hierarchy item to render the subpage teasers for
  * @param $propertiesToRender string optional: a comma separated list of property names to render for each subpage. Will be displayed in the order of appearance in the csv-string. If not provided a default set of properies will be rendered
  */
 class CmsSubpageTeaserWidget extends Widget {
-	
+
 	public $propertiesToRender = null;
 	/* @var $cmsHierarchyItem CmsHierarchyItem */
 	public $cmsHierarchyItem = null;
-	
+	public $renderCreationDate = false;
+
 	public function init() {
 		parent::init ();
 	}
-	
+
 	public function renderSubpageTeasers(&$widgetHtml){
-	    /* @var $cmsHierarchyItem CmsHierarchyItem */
-	    $cmsHierarchyItem = $this->cmsHierarchyItem;
-	    $cmsChildHierarchyItems = $cmsHierarchyItem->getCmsHierarchyItems()->where(['display_state' => CmsHierarchyItem::DISPLAYSTATE_PUBLISHED_VISIBLE_IN_NAVIGATION])->orderBy("position asc")->all();
-	    
-	    if(count($cmsChildHierarchyItems) > 0){
-	        $widgetHtml .= '<ul class="cms-subpage-teaser-list">'.PHP_EOL;
-	        
-	        foreach ($cmsChildHierarchyItems as $child){
-	            /* @var $child CmsHierarchyItem */
-	            $cmsMenus = $child->getCmsMenus()->all();  //TODO: probably need to determine proper lang version here by applying a where filter
-	            if(count($cmsMenus) > 0){
-	                /* @var $cmsMenuItem CmsMenuItem */
-	                $cmsMenuItem = $cmsMenus[0];
-	                /* @var $pageContent CmsPageContent */
-	                $pageContent = $cmsMenuItem->getPageContent()->one();
-    	            $widgetHtml .= '<li class="cms-subpage-teaser-item">'.PHP_EOL;
-    	            $widgetHtml .= '<span class="cms-subpage-teaser-item-date"><a href="'.$cmsMenus[0]->getFormattedUrl().'">'.date_format(date_create($pageContent->created_datetime),"d. M Y").'</a></span>';
-    	            $widgetHtml .= '<span class="cms-subpage-teaser-item-title"><a href="'.$cmsMenus[0]->getFormattedUrl().'">'.$cmsMenuItem->name.'</a></span>';
-    	            $widgetHtml .= '<span class="cms-subpage-teaser-item-description">'.$pageContent->description.'</span>';
-    	            $widgetHtml .= '<span class="cms-subpage-teaser-item-morelink"><a href="'.$cmsMenus[0]->getFormattedUrl().'">'.Yii::t('simplecms', 'read more...').'</a></span>';
-    	            $widgetHtml .= '</li>'.PHP_EOL;
-	            }
-	        }
-	        
-	        $widgetHtml .= '</ul>'.PHP_EOL;
-	    }
+		/* @var $cmsHierarchyItem CmsHierarchyItem */
+		$cmsHierarchyItem = $this->cmsHierarchyItem;
+		$cmsChildHierarchyItems = $cmsHierarchyItem->getCmsHierarchyItems()->where(['display_state' => CmsHierarchyItem::DISPLAYSTATE_PUBLISHED_VISIBLE_IN_NAVIGATION])->orderBy("position asc")->all();
+
+		if(count($cmsChildHierarchyItems) > 0){
+			$widgetHtml .= '<ul class="cms-subpage-teaser-list">'.PHP_EOL;
+
+			foreach ($cmsChildHierarchyItems as $child){
+				/* @var $child CmsHierarchyItem */
+				$cmsMenus = $child->getCmsMenus()->all();  //TODO: probably need to determine proper lang version here by applying a where filter
+				if(count($cmsMenus) > 0){
+					/* @var $cmsMenuItem CmsMenuItem */
+					$cmsMenuItem = $cmsMenus[0];
+					/* @var $pageContent CmsPageContent */
+					$pageContent = $cmsMenuItem->getPageContent()->one();
+					$widgetHtml .= '<li class="cms-subpage-teaser-item">'.PHP_EOL;
+					if($this->renderCreationDate) {
+						$widgetHtml .= '<span class="cms-subpage-teaser-item-date"><a href="' . $cmsMenus[0]->getFormattedUrl() . '">' . date_format(date_create($pageContent->created_datetime), "d.m.Y") . '</a></span>';
+					}
+					$widgetHtml .= '<span class="cms-subpage-teaser-item-title"><a href="'.$cmsMenus[0]->getFormattedUrl().'">'.$cmsMenuItem->name.'</a></span>';
+					$widgetHtml .= '<span class="cms-subpage-teaser-item-description">'.$pageContent->description.'</span>';
+					$widgetHtml .= '<span class="cms-subpage-teaser-item-morelink"><a href="'.$cmsMenus[0]->getFormattedUrl().'">'.Yii::t('simplecms', 'read more...').'</a></span>';
+					$widgetHtml .= '</li>'.PHP_EOL;
+				}
+			}
+
+			$widgetHtml .= '</ul>'.PHP_EOL;
+		}
 		return $widgetHtml;
 	}
-	
+
 	public function run() {
 		$widgetHtml = '<div class="cms-subpage-teasers">'.PHP_EOL;
-		
+
 		$this->renderSubpageTeasers($widgetHtml);
-		
+
 		$widgetHtml .= '</div>'.PHP_EOL;
 		return $widgetHtml;
 	}
 }
-?>
