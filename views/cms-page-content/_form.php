70e6b39527b9bd56cc07b9bad239b756721d6c8f
Paul Kerspe <paul.kerspe@unbelievable-machine.com>
2017-04-09 14:57:16 +0200

    cleaned up interface by grouping properties in collapsable panels

--- views/cms-page-content/_form.php
+++ views/cms-page-content/_form.php
@@ -26,22 +26,61 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'content')->textarea(['class' => 'ckeditor']) ?>
     <?= $form->field($model, 'render_subpage_teasers')->checkbox() ?>
 
-    <h3>Content Teaser details:</h3>
-    <?= $form->field($model, 'teaser_text')->textarea(['maxlength' => 500]) ?>
-    <!-- TODO: render a  image selector button to open the media browser and display preview of current selected image if any -->
-    <?= $form->field($model, 'teaser_image_id') ?>
-    <?= $form->field($model, 'teaser_name') ?>
-    <?= $form->field($model, 'teaser_link') ?>
+    <div class="panel-group">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" href="#collapseTeaserDetails">Page Teaser settings (click to open)</a>
+                </h4>
+            </div>
+            <div id="collapseTeaserDetails" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <?= $form->field($model, 'teaser_name') ?>
+                    <?= $form->field($model, 'teaser_text')->textarea(['maxlength' => 500]) ?>
+                    <!-- TODO: render a  image selector button to open the media browser and display preview of current selected image if any -->
+                    <?= $form->field($model, 'teaser_image_id') ?>
+                    <?= $form->field($model, 'teaser_link') ?>
+                </div>
+                <div class="panel-footer">In this section you can specify the teaser details to be used whenever the page content should be teasered on another page. E.g. you can specify the name and text excerpt to be displayed if this page is displayed in the subpage teaser list of its parent page.</div>
+            </div>
+        </div>
+    </div>
 
-    <h3>Meta tags:</h3>
-    <?= $form->field($model, 'description')->textarea(['maxlength' => 500]) ?>
-    <?= $form->field($model, 'html_title') ?>
-    <?= $form->field($model, 'meta_keywords')->textarea(['maxlength' => 255]) ?>
-    <?= $form->field($model, 'metatags_general')->textarea(['maxlength' => 500]) ?>
+    <div class="panel-group">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" href="#collapseMetaTags">Meta Tags for this page (click to open)</a>
+                </h4>
+            </div>
+            <div id="collapseMetaTags" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <?= $form->field($model, 'description')->textarea(['maxlength' => 500]) ?>
+                    <?= $form->field($model, 'html_title') ?>
+                    <?= $form->field($model, 'meta_keywords')->textarea(['maxlength' => 255]) ?>
+                    <?= $form->field($model, 'metatags_general')->textarea(['maxlength' => 500]) ?>
+                </div>
+                <div class="panel-footer">Edit meta tags that are rendered in the html header in this area. All this tags are optional but can be used for search engine optimization</div>
+            </div>
+        </div>
+    </div>
 
-    <h3>Advanced settings:</h3>
-    <?= $form->field($model, 'javascript')->textarea() ?>
-    <?= $form->field($model, 'css')->textarea() ?>
+    <div class="panel-group">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" href="#collapseAdvanced">Advanced settings (click to open)</a>
+                </h4>
+            </div>
+            <div id="collapseAdvanced" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <?= $form->field($model, 'javascript')->textarea() ?>
+                    <?= $form->field($model, 'css')->textarea() ?>
+                </div>
+                <div class="panel-footer">This section allows you to provide custom, page specific javascript and css code that will be rendered in the page</div>
+            </div>
+        </div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
